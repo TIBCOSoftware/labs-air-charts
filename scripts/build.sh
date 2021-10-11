cf729418657f8613bb6b73cf539e76584fc2acff
Miguel Torres <mtorres@MTORRES-MBP16.local>
2021-10-11 15:01:00 -0700

    Create makefile for build installer

--- scripts/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-destination=$1
-
-mkdir -p dist
-helm dependency update charts/air
-cp -r charts/air ${destination}
-cp scripts/delete_minikube.sh ${destination}
-cp scripts/install_minikube.sh ${destination}
