9f6a74186372ac7628e04d271b57c21530b62e22
Miguel Torres <migueltorreslopez@gmail.com>
2021-10-11 16:19:59 -0700

    Fix values path on installer

--- scripts/build_installer.sh
+++ scripts/build_installer.sh
@@ -5,8 +5,8 @@ installer_target_path="dist"
 if [ -d $installer_target_path ]; then
   rm -rf $installer_target_path
 fi
-
 mkdir -p $installer_target_path
+
 helm dependency update charts/air
 cp -r charts/air $installer_target_path
 cp scripts/delete_minikube.sh $installer_target_path
