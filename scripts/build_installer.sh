3396f05bf489f1fad7604541026f6bbfe149f629
Jeremy Dinsel <987246+Gerromie@users.noreply.github.com>
2022-01-20 09:45:42 -0800

    feat(infra): check for errors, refine platform code

--- scripts/build_installer.sh
+++ scripts/build_installer.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+if [ -e functions.sh ]; then
+    # shellcheck disable=SC1091
+    . functions.sh
+fi
+
 installer_target_path="dist"
 
-if [ -d $installer_target_path ]; then
-  rm -rf $installer_target_path
+if [ -d ${installer_target_path} ]; then
+  rm -rf ${installer_target_path}
 fi
-mkdir -p $installer_target_path
+mkdir -p ${installer_target_path} || exit 1
 
 # Minikube artifacts
 helm dependency update charts/air
-cp -r charts/air $installer_target_path
-cp scripts/delete_* $installer_target_path
-cp scripts/install_* $installer_target_path
+cp -r charts/air ${installer_target_path} || exit 1
+cp scripts/delete_* ${installer_target_path} || exit 1
+cp scripts/install_* ${installer_target_path} || exit 1
+cp scripts/functions.sh ${installer_target_path} || exit 1
 
 # Docker compose artifacts
-cp -r air-backend $installer_target_path
+cp -r air-backend ${installer_target_path} || exit 1
